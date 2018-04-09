b5a24fe04d80b576ca7aa2dc539bced1188c0661
J.D. Bean <jonathandbean@gmail.com>
2018-04-09 09:52:15 -0400

    basic solution

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -3,7 +3,18 @@ require 'pry-byebug'
 # but you may remove it if you like.
 
 def fibonacci(n)
-  # your code here :)
+    y = 1
+    z = 1
+    if n == 1 || n == 2
+      return 1
+    else
+      (n-2).times do
+        x = z + y
+        y = z
+        z = x
+      end
+      puts z
+    end
 end
 
 Pry.start
